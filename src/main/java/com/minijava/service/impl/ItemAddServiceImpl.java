334700a58709bbe357d80167b263590fdc0d9a5a
gusgh075 <gusgh075@gmail.com>
2025-12-06 20:15:03 +0900

    chore : 아이템 등록시 마지막 요소를 조회하는데, 요소가 없는 상황에 대한 로직 추가. if문 내부 처리로직 오류 해결

--- src/main/java/com/minijava/service/impl/ItemAddServiceImpl.java
+++ src/main/java/com/minijava/service/impl/ItemAddServiceImpl.java
@@ -15,20 +15,31 @@ public class ItemAddServiceImpl implements ItemAddService {
     }
 
 
+    /**
+     * item을 추가하는 메서드. return 값으로 ID를 반환한다. 0을 반환할 시 중복 값이 있다는 것.
+     *
+     * @param name
+     * @param rank
+     * @param quality
+     * @param power
+     * @return
+     */
     @Override
     public int itemAdd(String name, String rank, String quality, int power) {
         // 이름 중복 아이템 처리
         List<ItemDTO> byName = itemRepository.findByName(name);
-        if(byName.isEmpty()){
-            System.out.println("이름이 중복됩니다. 아이템을 다시 등록해주세요.");
-            for (ItemDTO itemDTO : byName) {
-                System.out.println("중복되는 아이템 정보 => "+itemDTO);
-            }
-            return 0;
-        }
+        if (!byName.isEmpty()) return -1;
+
         // 아이템 정상 등록
-        int newItemNumber = itemRepository.getLastItemNumber() + 1;
-        itemRepository.save(new ItemDTO(newItemNumber, name, rank, quality, power));
-        return newItemNumber;
+        int lastItemNumber = itemRepository.getLastItemNumber();
+        if (lastItemNumber == -1){ //repository에 아이템이 없는 경우
+            itemRepository.save(new ItemDTO(1, name, rank, quality, power));
+            return 1;
+        }
+        else {
+            int newItemNumber = lastItemNumber+1;
+            itemRepository.save(new ItemDTO(newItemNumber, name, rank, quality, power));
+            return newItemNumber;
+        }
     }
 }
